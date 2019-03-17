2c5ca2ee837335a57940910e3cd84d5a8e807c35
hirantha <sahanhirantha@gmail.com>
2019-03-17 20:38:58 +0530

    tooltip changed to help div

---
+++ home/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: hiruu
+ * Date: 3/15/19
+ * Time: 7:20 PM
+ */
+
+print_r($_POST);
+echo count($_POST);
--- index.php
+++ index.php
@@ -16,10 +16,15 @@
     <link rel="stylesheet" href="./stylesheets/main.css">
     <title>Login Section</title>
 </head>
-<body style="height: 97%">
+<body>
+
+<div class="nav-bar">
+    <div class="nav-bar-title">iALevel</div>
+</div>
 
 <div>
-    <form action="/home" method="get" class="forms" style="width: 300px;text-align: center;margin: auto">
+    <form action="./home/" method="post" class="forms" style="width: 300px;text-align: center;margin: 10px auto auto auto"
+          name="login-form">
         <br>
         <img src="./icons/user.png" style="width: 150px;color: white">
         <br><br><br>
@@ -33,13 +38,28 @@
         <br><br><br>
         <input type="submit" value="Login" style="width: 250px" class="form-button">
         <br><br>
-        <div class="tooltip">Need help?
-            <span class="tooltiptext">
-                This is the login non-authenticating login.This just implemented for archive 3 levels of access for the app.Enter your name and choose a role for login in.thank you!
-            </span>
-        </div>
+        <a href="#" class="help-link" onclick="showHelpText()">Need help? Click here</a>
     </form>
 
 </div>
+<div class="help-box" style="margin: 10px auto;width: 330px;" hidden id="help-box">
+    <p id="help-text"></p>
+</div>
+<script>
+    var visible = false;
+
+    function showHelpText() {
+        var help = "This is the login non-authenticating login.This just implemented for archive 3 levels of access for the app.Enter your name and choose a role for login in.thank you!";
+        if (visible) {
+            document.getElementById("help-text").innerHTML = "";
+            document.getElementById("help-box").hidden = true;
+            visible = false
+        } else {
+            document.getElementById("help-text").innerHTML = help;
+            document.getElementById("help-box").hidden = false;
+            visible = true;
+        }
+    }
+</script>
 </body>
 </html>
