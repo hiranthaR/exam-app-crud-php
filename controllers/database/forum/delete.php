9f7b68fcbef55eaf1739e0eea56f57328b2f4766
hirantha <sahanhirantha@gmail.com>
2019-03-18 20:42:44 +0530

    delete api done

--- controllers/database/forum/delete.php
+++ controllers/database/forum/delete.php
@@ -5,3 +5,23 @@
  * Date: 3/18/19
  * Time: 8:33 PM
  */
+
+require_once "./../connection.php";
+
+$tableForumAnswers = "answers";
+
+if ($connection) {
+    $answerID = $_POST['id'];
+    deleteAnswer($answerID);
+} else {
+    echo false;
+}
+
+function deleteAnswer($id)
+{
+    global $connection;
+    global $tableForumAnswers;
+
+    $sql = "DELETE FROM " . $tableForumAnswers . " WHERE answer_id=" . $id . ";";
+    echo mysqli_query($connection, $sql);
+}
